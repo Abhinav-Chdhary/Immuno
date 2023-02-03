5841acbc11a4aa12dcca641e654739abd4878101
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-02-03 16:53:19 +0530

    Spawner first update

--- Immuno/Assets/Scripts/AsteroidSpawner.cs
+++ Immuno/Assets/Scripts/AsteroidSpawner.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AsteroidSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Asteroid asteroidPrefab;
+    public float trajectoryVariance = 15.0f;
+    public float spawnRate = 2.0f;
+    public float spawnDistance = 15.0f;
+    public int spawnAmount = 1;
+    private void Start()
     {
-        
+        InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Spawn()
     {
-        
+        for(int i=0; i<spawnAmount; i++)
+        {
+            Vector3 spawnDirection = Random.insideUnitCircle.normalized * spawnDistance;
+            Vector3 spawnPoint = transform.position+spawnDirection;
+
+            float variance = Random.Range(-trajectoryVariance, trajectoryVariance);
+            Quaternion rotation = Quaternion.AngleAxis(variance, Vector3.forward);
+
+            Asteroid asteroid = Instantiate(asteroidPrefab, spawnPoint, rotation);
+            asteroid.size = Random.Range(asteroid.minSize, asteroid.maxSize);
+        }
+    }
+    private void SetTrajectory()
+    {
+
     }
 }
