c7f64f1da07476429af6088e8f1cc68a95b40736
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-02-06 18:46:40 +0530

    Update Player.cs

--- Immuno/Assets/Scripts/Player.cs
+++ Immuno/Assets/Scripts/Player.cs
@@ -51,4 +51,16 @@ public class Player : MonoBehaviour
         Bullet bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
         bullet.Project(transform.up);
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Asteroid")
+        {
+            _rigidbody.velocity = Vector3.zero;
+            //stopping movement
+            _rigidbody.angularVelocity = 0.0f;
+            //stopping rotation
+            gameObject.SetActive(false);
+            FindObjectOfType<GameManager>().PlayerDied();
+        }
+    }
 }
