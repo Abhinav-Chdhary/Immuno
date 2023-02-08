8b16f47db410575ebbc836473e748132c08842eb
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-02-08 12:31:59 +0530

    Explosions update

--- Immuno/Assets/Scripts/GameManager.cs
+++ Immuno/Assets/Scripts/GameManager.cs
@@ -2,13 +2,24 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public ParticleSystem playerExplosion;
+    public ParticleSystem asteroidExplosion;
     public Player player;
     public int lives = 3;
     public int score = 0;
     public float respawnTime = 3.0f;
 
+    public void AsteroidDestroyed(Asteroid asteroid)
+    {
+        score++;
+        asteroidExplosion.transform.position = asteroid.transform.position;
+        asteroidExplosion.Play();
+    }
     public void PlayerDied()
     {
+        playerExplosion.transform.position = player.transform.position;
+        playerExplosion.Play();
+
         lives--;
         if (lives <= 0)
         {
